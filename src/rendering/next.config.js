4544f08af2dbc5d3f2c3ec9de46f989a304bfe45
Balamurugan Srinivasan <balamurugan.s@altudo.co>
2023-07-14 14:04:26 +0530

    analyzer - enabled

--- src/rendering/next.config.js
+++ src/rendering/next.config.js
@@ -90,3 +90,13 @@ module.exports = () => {
 }
 
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  env: {
+      NEXT_PUBLIC_ENV: 'PRODUCTION', //your next configs goes here
+  },
+})
+
